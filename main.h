2ae5378c42d3b74c2f716b207466d022d32778a6
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-28 15:58:20 -0700

    printf header file

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <stdarg.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
 int put_char(char c);
+int pu_ts(char *s);
+
 
 #endif
