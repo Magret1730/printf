7d2a68a8e84f22a059d0cdbae9953b00f6c59b92
Magret <oyedeleabiodun79@gmail.com>
2023-05-28 00:30:21 -0700

    cretaed a putchar function

---
+++ README.md
@@ -0,0 +1,3 @@
+C - printf
+
+A group project between [Oyedele Abiodun Magret](https://github.com/Magret1730)
---
+++ put_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * put_char - prints character
+ * @c: argument to accept
+ *
+ * Description: A function that prints character
+ * Emulating the putchar standard library function
+ * Return: integer
+ */
+
+int put_char(char o)
+{
+	return (write(1, &o, 1));
+}
