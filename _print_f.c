433f05ad225ac24dc8ce90569a09556fb12f135f
Magret <oyedeleabiodun79@gmail.com>
2023-05-29 13:00:18 -0700

    function file to handle c,s,%

---
+++ _print_f.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _printf - prints according to a format
+ * @format: argument count
+ * Description: function that prints according to a format
+ * Return: count
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list argu;
+	int count = 0;
+	size_t i;
+	p_handler hand[] = {{"c", p_char}, {"s", p_str}};
+
+	va_start(argu, format);
+	if (format == NULL)
+	{
+		va_end(argu);
+		return (-1);
+	}
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			for (i = 0; i < sizeof(hand) / sizeof(hand[0]); i++)
+			{
+				if (*format == *hand[i].spec)
+				{
+					count += hand[i].p_func(argu);
+					break;
+				}
+			}
+			if (i == sizeof(hand) / sizeof(hand[0]))
+			{
+				write(1, "%", 1);
+				count ++;
+			}
+		}
+		format++;
+	}
+	va_end(argu);
+	return (count);
+}
