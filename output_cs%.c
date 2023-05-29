b753bdd2b4c688025193b8083ff259b467728a9a
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-28 17:30:48 -0700

    function that produces output according to a format s, c, %

--- output_cs%.c
+++ output_cs%.c
@@ -46,3 +46,40 @@ int _printf(const char *format, ...)
 	return (count);
 }
 
+/**
+ * pu_ts - the function prints string
+ * @s: string to be printed
+ * Description: the function print string
+ * It emulate the standard library puts
+ * Return: count
+ */
+
+int pu_ts(char *s)
+{
+	int x = 0, count = 0;
+
+	if (s == NULL)
+		return (0);
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		count += put_char(s[x]);
+	}
+	return (count);
+}
+
+
+/**
+ * put_char - prints character
+ * @o: argument to accept
+ *
+ * Description: A function that prints character
+ * Emulating the putchar standard library function
+ * Return: integer
+ */
+
+int put_char(char o)
+{
+	return (write(1, &o, 1));
+}
+
+
