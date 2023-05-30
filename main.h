23ad5710caccc28640e35f19b5def7eeae6da342
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-30 02:28:52 -0700

    printf header file

--- main.h
+++ main.h
@@ -1,13 +1,31 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
 int put_char(char c);
 int pu_ts(char *s);
+int (*print_specs(const char *format))(va_list);
+
+
+/**
+ * struct spec - struct for specifiers
+ * @h: character 
+ * @fun_ptr: fuunction handler
+ */
+typedef struct spec
+{
+	char *h;
+	int (*fun_ptr)(va_list);
+} specs;
+
+int p_char(va_list);
+int p_str(va_list);
+int p_cent(va_list);
 
 
 #endif
