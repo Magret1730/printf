2117cfb1a179beea8b67049117033cd4f137cfdf
Magret <oyedeleabiodun79@gmail.com>
2023-05-30 05:24:04 -0700

    function that handles format specifier %,c,s

--- output_cs%.c
+++ output_cs%.c
@@ -26,6 +26,11 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
+			if (*format == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
 			if (*format == 'c')
 			{
 				c = va_arg(args, int);
