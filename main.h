d74a59cadecad316affd1ff7655282210cbdd804
Magret <oyedeleabiodun79@gmail.com>
2023-05-30 04:35:21 -0700

    header file for _printf

--- main.h
+++ main.h
@@ -5,21 +5,6 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/**
- * struct p_handlers- handles the specifiers
- * @spec: specifier string
- * @p_func: function pointer to the corresponding print function
- *
- * Description: This struct associates format specifiers with their
- *              respective print functions.
- */
-
-typedef struct p_handlers
-{
-	char *spec;
-	int (*p_func)(va_list);
-} p_handler;
-
 
 int _printf(const char *format, ...);
 int put_char(char o);
